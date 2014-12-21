6769ce36dd47d689820bda12a5cb3a9c854e9493
Richard Sobota <richard.sobota@gmail.com>
2014-12-21 18:51:08 +0100

    Assignment solved

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,67 @@
-## Put comments here that give an overall description of what your
-## functions do
+##
+## This module provides functions to calculate and cache inversion of given
+## matrix. Caching allows to use caculated matrix repeatedly (e.g. inside
+## cycle) without need to calculate inversion every time again.
+##
+## Written by Richard Sobota as part of assignment 2 in "R Programming" course.
+##
 
-## Write a short comment describing this function
 
+## Creates object which holds matrix and its inverted value. Matrix should
+## be given as parameter, it will be stored for later use. Inverted value is
+## calculated and stored by function cacheSolve.
+##
 makeCacheMatrix <- function(x = matrix()) {
+    m <- NULL
+    
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    
+    get <- function() {
+        x
+    }
 
+    setsolve <- function(solved) {
+        m <<- solved
+    }
+
+    getsolve <- function() {
+        m
+    }
+
+    list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
-
+## Returns a matrix that is the inverse of previously set matrix. If there is
+## already calculated value, is it returned without recalculating.
+## Cache matrix created by function makeCacheMatrix should be
+## given as parameter.
+##
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## try to get calculated value
+    m <- x$getsolve()
+    
+    ## test if calculated value has been read
+    if(!is.null(m)) {
+        message("Getting cached inverted matrix")
+
+        ## inverted matrix already calculated, just return it
+        return(m)
+    }
+    
+    ## get matrix to be inverted
+    data <- x$get()
+    
+    ## calculate inverted matrix
+    m <- solve(data, ...)
+    
+    ## store calclated value
+    x$setsolve(m)
+    
+    m
 }
